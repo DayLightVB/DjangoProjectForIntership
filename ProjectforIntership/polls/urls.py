7755acf377ba4ce9c548556c6003e15c92def86c
DayLightVB <vlad.bajsha1337@gmail.com>
2021-01-03 20:21:10 +0200

    The interface has been changed, the ability to add offers to the site has been added. Added display of offers on the site.

--- ProjectforIntership/polls/urls.py
+++ ProjectforIntership/polls/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('klo/', views.klo, name='klo'),
+    path('', views.klo, name='klo'),
     path('offers/', views.offers, name='offers'),
     path('about/', views.about, name='about'),
+    path('create/', views.create, name='create'),
+    path('<int:pk>', views.AdDetailView.as_view(), name='ad-detail'),
+    path('<int:pk>/update', views.AdUpdateView.as_view(), name='ad-update'),
+path('<int:pk>/delete', views.AdDeleteView.as_view(), name='ad-delete')
 ]
--- ProjectforIntership/polls/views.py
+++ ProjectforIntership/polls/views.py
@@ -1,20 +1,61 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Ad
+from .forms import AdForm
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def klo(request):
-    return render(request, 'polls/klo.html')
+    return render(request, 'polls/home.html')
+
+
+class AdDetailView(DetailView):
+    model = Ad
+    template_name = 'polls/details_ad.html'
+    context_object_name = 'detail_ad'
+
+
+class AdUpdateView(UpdateView):
+    model = Ad
+    template_name = 'polls/create.html'
+
+    form_class = AdForm
+
+
+class AdDeleteView(DeleteView):
+    model = Ad
+    success_url = '/offers'
+    template_name = 'polls/ad-delete.html'
 
 
 def offers(request):
-    return render(request, 'polls/offers.html')
+    ad = Ad.objects.order_by('-date')
+    return render(request, 'polls/offersklo.html', {'ad': ad})
 
 
 def about(request):
-    return render(request, 'polls/about.html')
+    return render(request, 'polls/aboutklo.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = AdForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('offers')
+        else:
+            error = 'Error: The form has been filled out incorrectly'
+
+    form = AdForm()
+
+    data = {
+        'form': form,
+        'error': error,
+    }
+
+    return render(request, 'polls/create.html', data)
+
+
 
 
 
